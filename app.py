1bde2a4beac0f68544e298e43b4fff185affd13f
Liran C <liran.c.caduri@gmail.com>
2020-10-21 18:14:44 +0300

    feat: 404 page, add: try catch to all requests

--- app.py
+++ app.py
@@ -2,13 +2,12 @@ from flask import Flask, render_template, request, redirect, url_for
 import requests
 import json
 
-# TODO - add try catch to all request
-# TODO - make 404 page design
 # TODO - fix search design to match smartphones
-# TODO - make filters by data manipulation (needed making by hand) (filters: by rating out of 10, by actress, by genre ...)
+# TODO - make filters by data manipulation (filters: by rating out of 10, by actress)
 # TODO - show text when there is no result
 # TODO - add Cast
 # TODO - implement figur 
+# TODO - consider where to redirect to 404
 
 genres_list = [
         "Comedy",
@@ -39,34 +38,55 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/<int:page>')
 def index(page=0):
-    #http://api.tvmaze.com/shows
-    response = requests.get(f'http://api.tvmaze.com/shows?page={page}')
-    return render_template('index.html', data= json.loads(response.content), category="all", genres=genres_list)
+
+    try:
+        response = requests.get(f'http://api.tvmaze.com/shows?page={page}')
+    except:
+        #frontend will print out or show 404 page
+        data = None
+    else:
+        data= json.loads(response.content)
+    return render_template('index.html', data=data, category="all", genres=genres_list)
 
 
 @app.route('/search', methods = ['POST'])
 def search():
-    if request.method == 'POST':
-        result = request.form['show']
+   
+    result = request.form['show']
+    try:
         response = requests.get(f'http://api.tvmaze.com/search/shows?q={result}')
-    return render_template('search.html', data= json.loads(response.content), name=result)
+    except:
+        #frontend will print out or show 404 page
+        data = None
+    else:
+        data= json.loads(response.content)
+
+    return render_template('search.html', data=data, name=result)
 
 
 @app.route('/show/<int:show_id>')
 def show_page(show_id):
     url = f'http://api.tvmaze.com/shows/{show_id}'
-    response = requests.get(url)
 
-    images_data = requests.get(f'{url}/images')
-    images_data = json.loads(images_data.content)
-    images_reversed =(list(reversed(images_data)))
+    try:
+        response = requests.get(url)
+        images_data = requests.get(f'{url}/images')
+    except:
+        data = None
+        images_reversed=None
+    else:
+        data= json.loads(response.content)
+        images_data = json.loads(images_data.content)
+        images_reversed =(list(reversed(images_data)))
 
     background = None
-    for img in images_reversed:
-        if img['type'] == 'background':
-            background = img
 
-    return render_template('show_page.html', data= json.loads(response.content), background=background)
+    if images_reversed:
+        for img in images_reversed:
+            if img['type'] == 'background':
+                background = img
+
+    return render_template('show_page.html', data=data, background=background)
 
 
 @app.route('/genre/<string:genre>/<int:page>')
@@ -75,18 +95,27 @@ def genre_filter(genre, page=0):
     
     if genre.lower() == 'all':
         return redirect(url_for('index'))
-
-    response = requests.get(f'http://api.tvmaze.com/shows?page={page}')
-    data = json.loads(response.content)
+    try:
+        response = requests.get(f'http://api.tvmaze.com/shows?page={page}')
+    except:
+        data= None
+    else:
+        data = json.loads(response.content)
 
     filtered = []
 
-    for obj in data:
-        for g in obj['genres']:
-            if g.lower() == genre.lower():
-                filtered.append(obj)
+    if data:
+        for obj in data:
+            for g in obj['genres']:
+                if g.lower() == genre.lower():
+                    filtered.append(obj)
 
     return render_template('index.html', data=filtered , category=genre.title(), genres=genres_list)
+
+
+@app.route('/404')
+def not_found():
+    return render_template('404page.html')
 
 
 if __name__ == '__main__':
