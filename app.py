27bdb24234abd052048a858cdc4ac325d817b29c
Liran C <liran.c.caduri@gmail.com>
2020-10-25 11:20:59 +0200

    Backend bug fixes

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, abort
 import requests
 import json
 
 genres_list = [
+        "All",
         "Comedy",
         "Fantasy",
         "Crime",
@@ -30,12 +31,10 @@ def index(page=0):
     try:
         response = requests.get(f'http://api.tvmaze.com/shows?page={page}')
     except:
-        #frontend will print out or show 404 page
         data = None
     else:
         data= json.loads(response.content)
     return render_template('index.html', data=data, category="all", genres=genres_list, page=page)
-
 
 @app.route('/search', methods = ['POST'])
 def search():
@@ -44,13 +43,11 @@ def search():
     try:
         response = requests.get(f'http://api.tvmaze.com/search/shows?q={result}')
     except:
-        #frontend will print out
         data = None
     else:
         data= json.loads(response.content)
 
     return render_template('search.html', data=data, name=result)
-
 
 @app.route('/show/<int:show_id>')
 def show_page(show_id):
@@ -79,9 +76,11 @@ def show_page(show_id):
 
     return render_template('show_page.html', data=data, cast=list(actors_without_duplicates), background=background)
 
-
 @app.route('/genre/<string:genre>/<int:page>')
 def genre_filter(genre, page=0):
+    if genre not in genres_list:
+        abort(404, description="Resource not found")
+
     if genre.lower() == 'all':
         return redirect(url_for('index'))
     try:
@@ -102,6 +101,10 @@ def genre_filter(genre, page=0):
 
 @app.route('/<int:rating>/<int:page>')
 def rating_filter(rating, page):
+
+    if rating < 0 or rating > 10:
+        abort(404, description="Resource not found")
+
     try:
         response = requests.get(f'http://api.tvmaze.com/shows?page={page}')
     except:
@@ -119,16 +122,13 @@ def rating_filter(rating, page):
             if not(r is None):
                 if rating >= r < rating + 1:
                     filtered.append(obj)
-                
-
     
-    return render_template('raitingFooterComponent.html', data=filtered, rating=rating, category=f"rating {rating}", page=page)
-
+    return render_template('raitingFooterComponent.html', data=filtered, rating=rating, genres=genres_list, category=f"rating {rating}", page=page)
 
 @app.errorhandler(404)
 def not_found(e):
+    print(e)
     return render_template('404page.html'), 404
-
 
 if __name__ == '__main__':
     app.run()
